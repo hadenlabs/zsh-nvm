5c8978398122cb6d26b6683df7380cf787ace5b7
Luis Mayta <luis@hadenlabs.com>
2019-02-24 08:29:40 -0500

    fix: install and load dependences (0.1.1)

--- zsh-nvm.zsh
+++ zsh-nvm.zsh
@@ -8,13 +8,32 @@
 #   Luis Mayta <slovacus@gmail.com>
 #
 
+LIGHT_GREEN='\033[1;32m'
+GREEN='\033[0;32m'
+CLEAR='\033[0m'
+
 function install_nvm {
-    curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+    if [[ ! -e "$HOME/.nvm" ]]; then
+        echo -e "${CLEAR}${LIGHT_GREEN}Installing NVM${CLEAR}"
+        curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+        install_dependences
+    fi
+}
+
+function install_dependences {
+    [ -e "$HOME/.nvm" ] && export NVM_DIR="$HOME/.nvm"  # This loads nvm
+    load_nvm
+    echo -e "${CLEAR}${LIGHT_GREEN}Installing Dependences${CLEAR}"
+    nvm install 8.9.1
+    nvm use 8.9.1 --default
+    npm install --global lambda-pure-prompt
+    npm install --global pure-prompt
+
 }
 
 function load_nvm {
-    if [[ ! -e "$HOME/.nvm" ]]; then
-        [ -s "$HOME/.nvm" ] && export NVM_DIR="$HOME/.nvm"  # This loads nvm
+    if [[ -e "$HOME/.nvm" ]]; then
+        [ -e "$HOME/.nvm" ] && export NVM_DIR="$HOME/.nvm"  # This loads nvm
         [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
         [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
     fi
@@ -23,5 +42,5 @@ function load_nvm {
 load_nvm
 
 if (( ! $+commands[nvm] )); then
-    [ ! -s "$NVM_DIR" ] && install_nvm
+    install_nvm
 fi
