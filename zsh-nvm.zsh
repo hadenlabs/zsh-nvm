098eb831a3a3b1d84fdf0ab8acee8a20932a99e2
@slovacus <slovacus@gmail.com>
2019-06-08 21:30:21 -0500

    refactor: change name functions (#7)

--- zsh-nvm.zsh
+++ zsh-nvm.zsh
@@ -12,26 +12,27 @@ LIGHT_GREEN='\033[1;32m'
 GREEN='\033[0;32m'
 CLEAR='\033[0m'
 
-function install_nvm {
+function nvm::install {
     if [[ ! -e "$HOME/.nvm" ]]; then
         echo -e "${CLEAR}${LIGHT_GREEN}Installing NVM${CLEAR}"
         curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-        install_dependences
+        nvm::dependences
     fi
 }
 
-function install_dependences {
+function nvm::dependences {
     [ -e "$HOME/.nvm" ] && export NVM_DIR="$HOME/.nvm"  # This loads nvm
-    load_nvm
+    nvm::load
     echo -e "${CLEAR}${LIGHT_GREEN}Installing Dependences${CLEAR}"
-    nvm install 8.9.1
-    nvm use 8.9.1 --default
-    npm install --global lambda-pure-prompt
-    npm install --global pure-prompt
-
+    nvm install 10.16.0
+    nvm use 10.16.0 --default
+    npm install --global yarn
+    yarn global add lambda-pure-prompt
+    yarn global add pure-prompt
+    yarn global add prettier
 }
 
-function load_nvm {
+function nvm::load {
     if [[ -e "$HOME/.nvm" ]]; then
         [ -e "$HOME/.nvm" ] && export NVM_DIR="$HOME/.nvm"  # This loads nvm
         [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
@@ -39,8 +40,8 @@ function load_nvm {
     fi
 }
 
-load_nvm
+nvm::load
 
 if (( ! $+commands[nvm] )); then
-    install_nvm
+    nvm::install
 fi
