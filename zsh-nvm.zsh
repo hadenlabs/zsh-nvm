deb990a56cfbfc87b00c7c4848a67313fe95251d
Luis Mayta <luis@hadenlabs.com>
2019-11-20 16:46:28 -0500

    chore: implement libs for nvm (#20)

--- zsh-nvm.zsh
+++ zsh-nvm.zsh
@@ -8,24 +8,30 @@
 #   Luis Mayta <slovacus@gmail.com>
 #
 
-LIGHT_GREEN='\033[1;32m'
-CLEAR='\033[0m'
+package_name=nvm
 
+plugin_dir=$(dirname "${0}":A)
+
+# shellcheck source=/dev/null
+source "${plugin_dir}"/src/helpers/messages.zsh
+# shellcheck source=/dev/null
+source "${plugin_dir}"/src/helpers/tools.zsh
 
 function nvm::dependences {
-    echo -e "${CLEAR}${LIGHT_GREEN}Installing Dependences${CLEAR}"
+    message_info "Installing dependences for ${package_name}"
+    message_success "Installed dependences for ${package_name}"
 }
 
 function nvm::install {
     nvm::dependences
-    if [[ ! -e "${HOME}/.nvm" ]]; then
-        echo -e "${CLEAR}${LIGHT_GREEN}Installing NVM${CLEAR}"
-        curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-    fi
-    nvm::post_install
+    message_info "Installing ${package_name}"
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+    message_success "Installed ${package_name}"
 }
 
 function nvm::post_install {
+    message_info "Post Install ${package_name}"
+
     nvm install 12.13.0
     nvm install 10.16.3
 
@@ -35,10 +41,13 @@ function nvm::post_install {
        lambda-pure-prompt pure-prompt prettier \
        localtunnel typescript \
        next webpack
+
+    message_success "Success Install ${package_name}"
 }
 
 function nvm::load {
     if [ -e "${HOME}/.nvm" ]; then
+        PATH=$(get_path)
         [ -e "${HOME}/.nvm" ] && export NVM_DIR="${HOME}/.nvm"  # This loads nvm
         # shellcheck source=/dev/null
         [ -s "${NVM_DIR}/nvm.sh" ] && source "${NVM_DIR}/nvm.sh"  # This loads nvm
@@ -51,4 +60,5 @@ nvm::load
 
 if [ ! -e "${HOME}/.nvm" ]; then
     nvm::install
+    nvm::post_install
 fi
