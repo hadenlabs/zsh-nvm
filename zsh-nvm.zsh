80886aca018c67eb8d78c7f890594bd411d26abc
Luis Mayta <luis@hadenlabs.com>
2020-02-20 16:32:01 -0500

    chore: implement version 12.14.1 node (#45)

--- zsh-nvm.zsh
+++ zsh-nvm.zsh
@@ -49,8 +49,9 @@ function nvm::post_install {
         nvm::install
     fi
     message_info "Post Install ${nvm_package_name}"
-    nvm install 12.13.0
     nvm install 10.16.3
+    nvm install 12.13.0
+    nvm install 12.14.1
     nvm install --lts
     nvm use --lts --default
     message_success "Success Install ${nvm_package_name}"
